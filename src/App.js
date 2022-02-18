a480137a251dc3a1fdc8cf7554ff3d44d4c66e57
Atul Mane <maneatul225@gmail.com>
2022-02-18 14:46:37 +0530

    Build the slider component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
 function App() {
     return (
         <>
             <Navbar />
+            <Slider />
+            <div>Content</div>
         </>
     );
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "../assets/css/slider.css";
+import image1 from "../assets/images/ecom1.png";
+import image2 from "../assets/images/ecom2.png";
+import image3 from "../assets/images/ecom3.jpg";
+import { MdArrowLeft, MdArrowRight } from "react-icons/md";
+
+const images = [image1, image2, image3];
+
+const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const nextImg = () => {
+        if (slideIndex !== images.length - 1) setSlideIndex(slideIndex + 1);
+        else if (slideIndex === images.length - 1) setSlideIndex(0);
+    };
+
+    const prevImg = () => {
+        if (slideIndex !== 0) setSlideIndex(slideIndex - 1);
+        else if (slideIndex === 0) setSlideIndex(images.length - 1);
+    };
+
+    return (
+        <div className="slider">
+            <div className="arrow left">
+                <MdArrowLeft size={25} onClick={() => prevImg()} />
+            </div>
+            <div className="slider__image">
+                <img src={images[slideIndex]} alt="" />
+            </div>
+            <div className="arrow right">
+                <MdArrowRight size={25} onClick={() => nextImg()} />
+            </div>
+        </div>
+    );
+};
+
+export default Slider;
