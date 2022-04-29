c3391d487ad1f1990d91457754743b24f66ef0bb
Atul Mane <maneatul225@gmail.com>
2022-04-29 20:57:44 +0530

    Added addtocart feature

---
+++ src/components/BuyNowButton.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { toast } from "react-toastify";
+import { addToCart } from "../services/cartService";
+import { getCart } from "../services/cartService";
+import { getCurrentUser } from "../services/authService";
+import { useContext } from "react";
+import { QueryContext } from "../contexts/QueryContext";
+
+const handleClick = async (id, cartCount, setCartCount) => {
+    if (!getCurrentUser()) window.location = "/login";
+    const { data: cart } = await getCart();
+    for (let item of cart) {
+        if (id === item.productId) {
+            // item.quantity += 1;
+            // const { data } = await updateCart(item);
+            // if (data) toast.success(data);
+            window.location = "/cart";
+            return;
+            // toast.info("Product already exists in cart");
+            // return;
+        }
+    }
+    const product = await addToCart(id);
+    if (product) toast.success(product.data);
+    window.location = "/cart";
+};
+
+const BuyNowButton = ({ id }) => {
+    const { cartCount, setCartCount } = useContext(QueryContext);
+
+    return (
+        <>
+            <button
+                className="btn btn--outlined"
+                onClick={() => handleClick(id, cartCount, setCartCount)}
+            >
+                Buy Now
+            </button>
+        </>
+    );
+};
+
+export default BuyNowButton;
